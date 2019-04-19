04751efebe39ec471fc04938545a4f757e49417c
Barron Wei <barronwei@outlook.com>
2019-04-19 00:31:52 -0400

    ready for firebase ios

--- android/app/src/main/java/com/idyllic/MainApplication.java
+++ android/app/src/main/java/com/idyllic/MainApplication.java
@@ -3,12 +3,15 @@ package com.barronwei.idyllic;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,8 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNGestureHandlerPackage()
+          new VectorIconsPackage(),
+          new RNGestureHandlerPackage(),
+          new RNFirebasePackage(),
+          new RNFirebaseAuthPackage()
       );
     }
 
