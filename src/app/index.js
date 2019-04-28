7ac300a60139629907c837e06d54ebd8d5af8c5f
Barron Wei <barronwei@outlook.com>
2019-04-27 20:28:33 -0400

    firestore working

--- src/app/index.js
+++ src/app/index.js
@@ -1,34 +1,22 @@
 import React, { Component } from 'react'
-import { Alert } from 'react-native'
 import firebase from 'react-native-firebase'
 import { navigate, set } from './services/navigation'
 import Initial from './build'
 
 export default class App extends Component {
   componentDidMount() {
+    this.pers = firebase.firestore().settings({ persistence: true })
     this.auth = firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.data = firebase
-          .firestore()
-          .collection('users')
-          .doc(user.uid)
-          .onSnapshot(
-            () => {
-              Alert.alert('Change')
-            },
-            error => Alert.alert(error.message)
-          )
         navigate('Home')
       } else {
         navigate('Open')
       }
     })
-    this.pers = firebase.firestore().enablePersistence
   }
   componentWillUnmount() {
-    this.auth()
     this.pers()
-    this.data()
+    this.auth()
   }
   tackle = ref => {
     set(ref)
