5a9eb21a79255361caae9d4022a873554bd207e3
Barron Wei <barronwei@outlook.com>
2019-05-28 20:37:55 -0400

    abstract slider

--- src/app/views/Home/Cause/build.js
+++ src/app/views/Home/Cause/build.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import { Alert } from 'react-native'
-import Slider from 'react-native-slider'
 import { Input, Words } from '../../../styles'
-import { wp, hp } from '../../../services/visual'
+import { Slide } from '../../../services/build'
 
 export default class Form extends Component {
   constructor(props) {
@@ -14,15 +13,16 @@ export default class Form extends Component {
       power: 50
     }
   }
-  submit = async (task, time, power) => {
+  submit = async (task, time, prior, power) => {
     try {
-      Alert.alert(power)
+      Alert.alert(prior + power)
     } catch (error) {
       Alert.alert(error.message)
     }
   }
   render() {
     const { task, time, prior, power } = this.state
+
     return (
       <React.Fragment>
         <Input
@@ -39,28 +39,18 @@ export default class Form extends Component {
           value={time}
         />
         <Words>Priority</Words>
-        <Words>{prior}</Words>
-        <Slider
-          style={{ width: wp(60), height: hp(5) }}
+        <Slide
           minimumValue={1}
           maximumValue={5}
           step={1}
-          minimumTrackTintColor="black"
-          maximumTrackTintColor="gray"
-          thumbStyle={{ width: hp(1.5), height: hp(1.5) }}
           onValueChange={prior => this.setState({ prior })}
           value={prior}
         />
         <Words>Energy</Words>
-        <Words>{power}</Words>
-        <Slider
-          style={{ width: wp(60), height: hp(5) }}
+        <Slide
           minimumValue={10}
           maximumValue={90}
           step={5}
-          minimumTrackTintColor="black"
-          maximumTrackTintColor="gray"
-          thumbStyle={{ width: hp(1.5), height: hp(1.5) }}
           onValueChange={power => this.setState({ power })}
           value={power}
         />
