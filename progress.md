71345a861857ca0ee8c0905338868be9a56ed845
Barron Wei <barronwei@outlook.com>
2019-06-09 13:15:11 +0200

    clear

--- progress.md
+++ progress.md
@@ -125,7 +125,7 @@
 # 0.0.6
 
 - Added potential features markdown file
-- Added partition function to logic to separate set and mutable tasks
+- Added lodash package for manipulation of arrays
 
 ### Bugs
 
--- src/app/services/logic/index.js
+++ src/app/services/logic/index.js
@@ -1,3 +1,6 @@
+import { orderBy, partition } from 'lodash'
+import moment from 'moment'
+
 export function check(cs) {
   const hold = []
   cs.forEach(c => {
@@ -8,12 +11,12 @@ export function check(cs) {
   return hold.shift()
 }
 
-export function split(co, pr) {
-  return co.reduce((r, v) => (r[pr(v) ? 0 : 1].push(v), r), [[], []])
-}
-
 export function order(co, ts) {
-  if (!co) {
-    const [x, y] = split(ts, ts.shift)
-  }
+  const [xs, ys] = partition(ts, ts.shift)
+  const oxs = orderBy(xs, ['prior'], ['desc'])
+  oxs.forEach(ox => ({
+    ...ox,
+    ss: moment(ox.start),
+    se: moment(ox.start).add(ox.time)
+  }))
 }
